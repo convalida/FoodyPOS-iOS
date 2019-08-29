b88039a1670f02ed1f62c375ab2b6075dc86192f
Minakshi Sadana <minakshi.sadana@convalidatech.com>
2019-08-29 11:20:10 +0530

    Connectivity, AllBestSellerCell, BestSellerCell

--- FoodyPOS/Resources/API/Connectivity.swift
+++ FoodyPOS/Resources/API/Connectivity.swift
@@ -10,11 +10,13 @@
 import Foundation
 import Alamofire
 
+///Class to check if internet is connected.
 class Connectivity {
     
+    ///Message which is displayed when internet is not connected.
     static let msgNoNetwork = "The Internet connection appears to be offline"
     
-    //  Check if device is connected to internet
+    //  Check if device is connected to internet which uses NetworkReachabilityManager() which is pre defined class
     class var isConnectedToInternet:Bool {
         return NetworkReachabilityManager()!.isReachable
     }
--- FoodyPOS/View/Cell/BestSellerCell.swift
+++ FoodyPOS/View/Cell/BestSellerCell.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 
+/**
+This class is used for creating view inside cell in BestSellerVC.
+*/
 class BestSellerCell: UITableViewCell {
 
+    ///Outlet for title which displays weekly bestseller items, monthly bestseller items and yearly bestseller items for each corresponding section
     @IBOutlet weak var lblTitle: UILabel!
+    ///Outlet for item 1 name
     @IBOutlet weak var lblItem1: UILabel!
+    ///Outlet for item 2 name
     @IBOutlet weak var lblItem2: UILabel!
+    ///Outlet for item 3 name
     @IBOutlet weak var lblItem3: UILabel!
+    ///Outlet for count of item 1 
     @IBOutlet weak var lblValue1: UILabel!
+    ///Outlet for count of item 2
     @IBOutlet weak var lblValue2: UILabel!
+    ///Outlet for count of item 3
     @IBOutlet weak var lblValue3: UILabel!
+    ///Outlet for See All button
     @IBOutlet weak var btnAll: UIButton!
     
+    /**
+     Prepares the receiver for service after it has been loaded from an Interface Builder archive, or nib file.
+    */
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    /**
+    Sets the selected state of the cell, optionally animating the transition between states.
+    */
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
